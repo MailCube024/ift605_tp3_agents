adc0b619fca859e1d9ee4fbe4425d0dbfedd9d71
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-11-18 00:20:48 -0500

    Added wakeup behavior to simulate long requests failing. Corrected sinus parser.

--- src/main/java/com/ift605/tp3/jade/agents/client/ClientAgent.java
+++ src/main/java/com/ift605/tp3/jade/agents/client/ClientAgent.java
@@ -2,6 +2,7 @@ package com.ift605.tp3.jade.agents.client;
 
 import com.ift605.tp3.client.DerivationForm;
 import com.ift605.tp3.constants.ClientConstants;
+import com.ift605.tp3.jade.agents.client.behaviours.ClientCancelRequestWakerBehaviour;
 import com.ift605.tp3.jade.agents.client.behaviours.ClientListenerBehaviour;
 import com.ift605.tp3.jade.helper.DispatcherFinder;
 import com.ift605.tp3.jade.messages.EquationMessage;
@@ -46,12 +47,18 @@ public class ClientAgent extends GuiAgent {
                 logger.info("Derivation command received. Sending to system...");
                 Equation eq = (Equation) guiEvent.getParameter(0);
                 AID dispatcher = DispatcherFinder.getDispatcherAID(this);
-                send(request().to(dispatcher).withContent(new EquationMessage(getAID(),eq)).build());
+                ClearMessageQueue();
+                send(request().to(dispatcher).withContent(new EquationMessage(getAID(), eq)).build());
+                addBehaviour(new ClientCancelRequestWakerBehaviour(this, 1000 * 15));
                 break;
             case ClientConstants.RESPONSE:
                 logger.info("Received derivation from system. Sending output");
                 AbstractEquation derivated = (AbstractEquation) guiEvent.getParameter(0);
                 form.receiveDerivation(derivated);
+                break;
+            case ClientConstants.NORESPONSE:
+                logger.info("Timeout expired. Cancelling derivation request.");
+                form.logMessage("Could not derivate last equation.");
                 break;
             case ClientConstants.SHUTDOWN:
                 logger.info("Closing event received. Requesting closure of agent to platform.");
@@ -60,5 +67,9 @@ public class ClientAgent extends GuiAgent {
             default:
                 return;
         }
+    }
+
+    private void ClearMessageQueue() {
+        while(receive() != null);
     }
 }
---
+++ src/main/java/com/ift605/tp3/jade/agents/client/behaviours/ClientCancelRequestWakerBehaviour.java
@@ -0,0 +1,30 @@
+package com.ift605.tp3.jade.agents.client.behaviours;
+
+import com.ift605.tp3.constants.ClientConstants;
+import jade.core.Agent;
+import jade.core.behaviours.WakerBehaviour;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Michaël on 11/17/2015.
+ */
+public class ClientCancelRequestWakerBehaviour extends WakerBehaviour {
+    private static Logger logger = LoggerFactory.getLogger(ClientCancelRequestWakerBehaviour.class);
+
+    private final GuiAgent agent;
+
+    public ClientCancelRequestWakerBehaviour(GuiAgent a, long timeout) {
+        super(a, timeout);
+        this.agent = a;
+    }
+
+    @Override
+    protected void onWake() {
+        logger.info("Wake up client. Cancel response for equation.");
+        GuiEvent ge = new GuiEvent(this, ClientConstants.NORESPONSE);
+        agent.postGuiEvent(ge);
+    }
+}
